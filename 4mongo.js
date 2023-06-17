b4dc3c40d1440048bc915fda53335d91423cc9bc
Ayush4958 <98166557+Ayush4958@users.noreply.github.com>
2023-06-18 00:08:16 +0530

    Updating the Data in MongoDB

--- 4mongo.js
+++ 4mongo.js
@@ -1,5 +1,7 @@
 // Updating the data in mongodb 
 
+// Updating Single line Data
 db.items.updateOne({name:"samsung"} , {$set: {price:25}})  // Changing the price to the 25 of date who's name is samsung
 
+// Updating the Multiple line Data
 db.items.updateMany( {name:"I-phone"} , {$set: {price:1 , rating:0.5} } ) //updating price and rating of I-phone  
