4be41dfbc3d3818a0c752b28ab6266c5c74641f2
Ayush Bhandari <bhandariayush935@gmail.com>
2025-08-02 23:17:49 +0530

    Delete 3mongo.js

--- 3mongo.js
+++
@@ -1,7 +0,0 @@
-// Deleting the data from mongo db
-
-// Deleting the single data 
-db.items.deleteOne({price:1}) //deleteOne will delete the first matching entry in case of multi document match
-
-// Deleting the Multiple Data
-db.items.deleteMany({rating: {$st:2}})
